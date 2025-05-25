a44ca2549986368d7dc60da7eb1b069fc97d8d04
Dominik Ivancic <divancic@ffos.hr>
2025-05-25 23:37:12 +0200

    Instaliran Vita PWA plugin, napravljen manifest.webmanifest, konfiguriran vite.config.js i dodan link na manifest i meta tagove u index.html

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['ikona.png'],
+      manifest: {
+        name: 'CS Skins App',
+        short_name: 'CSApp',
+        description: 'PWA za pregled CS skinova',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'ikona.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'ikona.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
-
